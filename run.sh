f10d42d30aaf0bf9eaa6808fe462d6f077a806b4
Senol E <whereimdb@gmail.com>
2023-05-23 21:14:59 -0500

    run scrit updated for gunicorn

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
-python3 -m venv venv
-source venv/bin/activate
+#python3 -m venv venv
+#source venv/bin/activate
 pip3 install -r requirements.txt
-FLASK_APP=app
-flask run --host 0.0.0.0 --port 5000 --reload
+gunicorn app:app -b 0.0.0.0:5000
+#FLASK_APP=app
+#flask run --host 0.0.0.0 --port 5000 --reload
